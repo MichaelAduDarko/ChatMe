c1e913423a6f5fe3ac9d2977cdeb3e4b5c25234e
michaeladudarko <adudarkomichael@gmail.com>
2020-03-24 12:19:14 -0400

    Initial Commit

---
+++ ChatMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMe
+//
+//  Created by MICHAEL ADU DARKO on 3/24/20.
+//  Copyright © 2020 Bronzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
