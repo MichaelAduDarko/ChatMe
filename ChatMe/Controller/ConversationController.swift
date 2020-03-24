b225ca232a41f1a2b8757f5bbb3edebdec018fd9
michaeladudarko <adudarkomichael@gmail.com>
2020-03-24 13:11:55 -0400

    Setup Messages TbaleView

--- ChatMe/Controller/ConversationController.swift
+++ ChatMe/Controller/ConversationController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationController: UIViewController {
     
     //MARK:- Properties
+    private let tableView = UITableView()
     
     //MARK:- LifeCycle
     
@@ -26,17 +29,31 @@ class ConversationController: UIViewController {
     }
     
     
-    
     //MARK:-Helpers
     
     func configureUI(){
         view.backgroundColor = .white
         
        configureNavigationBar()
-       
+       configureTableView()
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain,
                                                            target: self, action: #selector(showProfile))
+        
+    }
+    
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+        
         
     }
     
@@ -57,4 +74,24 @@ class ConversationController: UIViewController {
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
     
+}
+
+extension ConversationController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Text Cell"
+        return cell
+        
+    }
+}
+
+
+extension ConversationController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
