904cfda5886f39438e938088c72e29b13cc90d9f
michaeladudarko <adudarkomichael@gmail.com>
2020-03-28 04:16:19 -0400

    UserCell Ui set up

--- ChatMe/Controller/NewMessageController.swift
+++ ChatMe/Controller/NewMessageController.swift
@@ -33,7 +33,7 @@ class NewMessageController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleDismissal))
         
         tableView.tableFooterView = UIView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
     }
 }
@@ -46,8 +46,7 @@ extension NewMessageController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        cell.textLabel?.text = "Text Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
         return cell
         
     }
