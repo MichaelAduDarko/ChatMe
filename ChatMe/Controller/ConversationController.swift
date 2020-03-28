14f6d9e934f59223b5359277fa27b82ecd08271f
michaeladudarko <adudarkomichael@gmail.com>
2020-03-28 00:36:51 -0400

    Keeping user logged in

--- ChatMe/Controller/ConversationController.swift
+++ ChatMe/Controller/ConversationController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "ConversationCell"
 
@@ -20,16 +21,43 @@ class ConversationController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        authenticateUser()
     }
-    
     
     //MARK:- Selectors
     @objc func showProfile(){
-        print("1234")
+       logOut()
     }
     
+    //MARK:-API
+    
+    func authenticateUser(){
+        if Auth.auth().currentUser?.uid == nil {
+            presentLoginScreen()
+        } else {
+            print("DEBUG: User id is \(Auth.auth().currentUser?.uid)")
+        }
+    }
+    
+    func logOut(){
+        do {
+            try Auth.auth().signOut()
+        } catch  {
+            print("DEBUG: Error Signnig out")
+        }
+    }
     
     //MARK:-Helpers
+    
+    func presentLoginScreen(){
+        DispatchQueue.main.async {
+            let controller = LoginController()
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav, animated: true, completion: nil)
+      
+        }
+    }
     
     func configureUI(){
         view.backgroundColor = .white
--- ChatMe/SceneDelegate.swift
+++ ChatMe/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let nav = UINavigationController(rootViewController: LoginController())
+        let nav = UINavigationController(rootViewController: ConversationController())
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
